d9e8a9592aebb2b92ae40680cd54252700996148
Mohammed Abdur Rahman <44206264+codezart@users.noreply.github.com>
2020-09-26 08:12:15 +0300

    fix for making it run everytime you start windows.

--- eyecare.py
+++ eyecare.py
@@ -2,6 +2,21 @@
 from win10toast import ToastNotifier
 import time
 import keyboard as kb
+# create bat file on windows
+import getpass
+import os
+
+USER_NAME = getpass.getuser()
+
+def add_to_startup(file_path=""):
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "eyecare.bat", "w") as bat_file:
+        bat_file.write(r'python %s\eyecare.py' % file_path)
+
+add_to_startup()
+
 # create an object to ToastNotifier class
 n = ToastNotifier()
 exit_key_not_pressed = True
