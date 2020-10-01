2b79828dfdf9297f3fff66e6f5f3ab389274f6fb
Mohammed Abdur Rahman <44206264+codezart@users.noreply.github.com>
2020-10-01 20:01:25 +0300

    tiny update

--- eyecare.py
+++ eyecare.py
@@ -1,21 +1,6 @@
 # import win10toast
 from win10toast import ToastNotifier
 import time
-import keyboard as kb
-# create bat file on windows
-import getpass
-import os
-
-USER_NAME = getpass.getuser()
-
-def add_to_startup(file_path=""):
-    if file_path == "":
-        file_path = os.path.dirname(os.path.realpath(__file__))
-    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
-    with open(bat_path + '\\' + "eyecare.bat", "w") as bat_file:
-        bat_file.write(r'python %s\eyecare.py' % file_path)
-
-add_to_startup()
 
 # create an object to ToastNotifier class
 n = ToastNotifier()
